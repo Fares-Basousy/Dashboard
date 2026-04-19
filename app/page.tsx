8e3b166077d1000fee1575704e01828a9f168785
Fares El-Basoysy <faresbasousy@gmail.com>
2026-04-19 12:42:11 +0200

    Nextjs Dashboard app

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 import Link from 'next/link';
+import styles from '../home.module.css';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className={styles.shape}>
         {/* <AcmeLogo /> */}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
@@ -25,7 +27,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Dashboard preview illustration on mobile"
+          />
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Dashboard preview illustration"
+          />
         </div>
       </div>
     </main>
