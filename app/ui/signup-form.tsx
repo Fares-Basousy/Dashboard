72da276f9c8caf82c9112c6695c1c703f4b3f4d0
Fares El-Basoysy <faresbasousy@gmail.com>
2026-04-27 17:05:29 +0300

    refactor: implement Suspense fallbacks for dashboard components and migrate auth forms to server components

--- app/ui/signup-form.tsx
+++ app/ui/signup-form.tsx
@@ -9,15 +9,15 @@ import {
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from '@/app/ui/button';
 import { useActionState } from 'react';
-import { authenticate } from '@/app/lib/actions';
+import { CreateUser } from '@/app/lib/actions';
 
-export default function LoginForm({
+export default function SignupForm({
   callbackUrl,
 }: {
   callbackUrl: string;
 }) {
   const [errorMessage, formAction, isPending] = useActionState(
-    authenticate,
+    CreateUser,
     undefined,
   );
 
@@ -25,9 +25,28 @@ export default function LoginForm({
     <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
+          Create Account.
         </h1>
         <div className="w-full">
+          <div>
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="name"
+            >
+              Name
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="name"
+                type="text"
+                name="name"
+                placeholder="Enter your full name"
+                required
+              />
+              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+          </div>
           <div>
             <label
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
@@ -70,7 +89,7 @@ export default function LoginForm({
         </div>
         <input type="hidden" name="redirectTo" value={callbackUrl} />
         <Button className="mt-4 w-full" aria-disabled={isPending}>
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+          Create Account <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
         <div
           className="flex h-8 items-end space-x-1"
@@ -80,7 +99,7 @@ export default function LoginForm({
           {errorMessage ? (
             <>
               <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-              <p className="text-sm text-red-500">{errorMessage}</p>
+              <p className="text-sm text-red-500">{errorMessage.message}</p>
             </>
           ) : null}
         </div>
